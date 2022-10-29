a1d039988fccc0340dcc7a53d01ad82b53165479
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-10-29 17:57:46 +0200

    fixing

--- pages/cats/index.js
+++ pages/cats/index.js
@@ -1,5 +1,6 @@
 import CatCard from "../../components/cat-card/cat-card.component.jsx";
 import styles from "../../styles/cats.module.scss";
+import { Head } from "next/document.js";
 import { Children } from "react";
 
 export async function getStaticProps(){
@@ -14,12 +15,18 @@ export async function getStaticProps(){
 function Cats({cats}) {
     const catsCards=Children.toArray(cats.map(({image,id,name},index)=><CatCard isMain={true} img={image?.url || "/image 1.png" } name={name} width="250" height="250" href={id} />))
     return (
+        <>
+        <Head>
+            <title>All Cats</title>
+            <meta name="description" content="Cats wiki | all cats | Most searched cats" />
+        </Head>
         <div>
             <h1 className='heading bold large dark'>66+ Breeds For you to discover</h1>
             <div className={styles.container}>
                 {catsCards}
             </div>
         </div>
+        </>
     );
 }
 
