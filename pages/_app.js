c12010fb6c95610ee92cbeaa20e756b262a98088
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-10-29 22:04:44 +0200

    customizing framer motion

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import { useRouter } from 'next/router';
 const variants = {
   hidden: { opacity: 0, x: -200, y: 0 },
   enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: -200, y: 0 },
+  exit: { opacity: 0, x: 200, y: 0 },
 };
 
 function MyApp({ Component, pageProps }) {
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <AnimatePresence mode='wait' initial={true} onExitComplete={()=> window.scrollTo(0,0)}>
-        <motion.main key={router.pathname==="/"?"home":router.pathname} variants={variants} initial="hidden" animate="enter" exit="exit" transition={{type:"linear"}}>
+        <motion.main key={router.pathname==="/"?"home":router.pathname} variants={variants} initial="hidden" animate="enter" exit="exit" transition={{type:"easeOut"}}>
           <Component {...pageProps} />
         </motion.main>
       </AnimatePresence>
