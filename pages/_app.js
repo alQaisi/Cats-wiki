ef261357e7398a9d9200e23855f17da6b724dae1
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-11-01 00:21:09 +0200

    testing fixing FOCU

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import Layout from '../components/layout/layout.component';
 import '../styles/globals.scss';
 import { AnimatePresence,motion } from "framer-motion"
 import { useRouter } from 'next/router';
+import Router from 'next/router';
+import { useEffect } from 'react';
 
 const variants = {
   hidden: { opacity: 0, x: -200, y: 0 },
@@ -11,10 +13,24 @@ const variants = {
 
 function MyApp({ Component, pageProps }) {
   const router =useRouter();
+  function routeChange(){
+    (function(){
+      const allstylesElems=document.querySelectorAll('style[media="x"]');
+      allstylesElems.forEach(elem=>elem.removeAttribute("media"));
+    })();
+  }
+  useEffect(() => {
+    Router.events.on('routeChangeComplete', routeChange);
+    Router.events.on('routeChangeStart', routeChange);
+    return () => {
+        Router.events.off('routeChangeComplete', routeChange);
+        Router.events.off('routeChangeStart', routeChange);
+    };
+  }, []);
   return(
     <Layout>
       <AnimatePresence mode='wait' initial={true} onExitComplete={()=> window.scrollTo(0,0)}>
-        <motion.main key={router.pathname==="/"?"home":router.pathname} variants={variants} initial="hidden" animate="enter" transition={{type:"linear"}}>
+        <motion.main key={router.pathname==="/"?"home":router.pathname} variants={variants} initial="hidden" animate="enter" exit="exit" transition={{type:"linear"}}>
           <Component {...pageProps} />
         </motion.main>
       </AnimatePresence>
