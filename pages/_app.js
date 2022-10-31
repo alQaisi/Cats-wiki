e6902d99ac570c1e6850932891061f2cb7ce2e1c
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-11-01 00:12:55 +0200

    edit transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import Layout from '../components/layout/layout.component';
-import Head from 'next/head';
 import '../styles/globals.scss';
 import { AnimatePresence,motion } from "framer-motion"
 import { useRouter } from 'next/router';
@@ -7,7 +6,7 @@ import { useRouter } from 'next/router';
 const variants = {
   hidden: { opacity: 0, x: -200, y: 0 },
   enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: -400, y: 0 },
+  exit: { opacity: 0, x: 200, y: 0 },
 };
 
 function MyApp({ Component, pageProps }) {
@@ -15,7 +14,7 @@ function MyApp({ Component, pageProps }) {
   return(
     <Layout>
       <AnimatePresence mode='wait' initial={true} onExitComplete={()=> window.scrollTo(0,0)}>
-        <motion.main key={router.pathname==="/"?"home":router.pathname} variants={variants} initial="hidden" animate="enter" exit="exit" transition={{type:"linear"}}>
+        <motion.main key={router.pathname==="/"?"home":router.pathname} variants={variants} initial="hidden" animate="enter" transition={{type:"linear"}}>
           <Component {...pageProps} />
         </motion.main>
       </AnimatePresence>
