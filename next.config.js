7a52010a79245bab2c4c22ddfac0807b4f57cc86
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-12-26 15:37:24 +0200

    initializing version 2

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env:{
-    CAT_API_KEY:process.env.CAT_API_KEY
-  },
-  images:{
-    domains:["cdn2.thecatapi.com"]
-  }
 }
 
 module.exports = nextConfig
